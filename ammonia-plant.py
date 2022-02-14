ce6ace99d0eba22701a3cd2a0d1503f3415c6c9d
BenPetersRAL <benjamin.peters@stfc.ac.uk>
2022-02-14 16:28:46 +0000

    .ini file added and hardcoded variables removed from main

---
+++ ammonia-plant.py
@@ -0,0 +1,226 @@
+import os
+from isobaric_reactor import *
+import matplotlib.pyplot as plt
+import math
+import numpy as np
+import copy
+import pyromat as pm
+import argparse
+from utils.options import Config
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "-c",
+    "--configuration",
+    help="specify the '*.ini' file path that configures the model")
+args = parser.parse_args()
+#  future would be to add output options file too
+
+def main():
+    """ Main to run the ammonia plant"""
+    
+    if args.configuration is None:
+        fcfg = os.path.join(os.path.dirname(__file__),'utils','default_options.ini')
+    else:
+        fcfg = args.configuration # pathname from input argument
+    cfg = Config(fcfg) # initialise 
+
+    # initial inputs - define bed structure, quench ratio
+    bed1 = Bed(cfg.reactor_length,
+               cfg.reactor_diameter,
+               cfg.reactor_mini,
+               False)
+    bed1.mass()
+
+    # set reactor inlet temp
+    reactor_in_temp = cfg.reactor_T_1_0
+
+    # input mols of n2 and h2
+    total_mol_N2 = 0.1   # mol
+    total_mol_H2 = total_mol_N2 * 3 # mol
+
+    # initialise iteration limits
+    inlet_temp = cfg.reactor_T_0_0
+    HTHE_DelT = reactor_in_temp - inlet_temp
+    HTHE_DelT_resid = cfg.reactor_dT_res
+    recycle_estimate = cfg.recycle
+
+    #initialise power consumption dictionary
+    power_consumption = {}
+
+
+    # make n2 and h2 input lines from psa and electrolysis functions
+    [Pipe_N2_LP, power_consumption["PSA"]] = psa_estimate(cfg.n2)                    # MAYBE ADD AIR_IN?
+    [Pipe_H2_LP, power_consumption["electrolysis"],Water_in] = electrolysis(cfg.h2)
+
+    print('H2 LP Feed = %3.1f' % Pipe_H2_LP.T, 'K\n')
+    print('N2 LP Feed = %3.1f' % Pipe_N2_LP.T, 'K\n')
+
+    # compress N2 and H2 lines
+
+    [Pipe_N2_HP, power_consumption["N2_comp"],heatlost1] = ptcompressor(Pipe_N2_LP,
+                                                                    cfg.reactor_P_0,
+                                                                    t_out=cfg.reactor_T_0_0+cfg.n2compressor_dT)
+    [Pipe_H2_HP, power_consumption["H2_comp"],heatlost2] = ptcompressor(Pipe_H2_LP,
+                                                                    cfg.reactor_P_0,
+                                                                    t_out=cfg.reactor_T_0_0+cfg.h2compressor_dT)
+
+    print('H2 HP Feed = %3.1f' % Pipe_H2_HP.T, 'K\n')
+    print('N2 HP Feed = %3.1f' % Pipe_N2_HP.T, 'K\n')
+
+    # mix H2 and N2 lines
+    Pipe_IN = mixer(Pipe_H2_HP,Pipe_N2_HP)
+
+    print('Mixed Feed pre-cooling = %3.1f' % Pipe_IN.T, 'K\n')
+
+    [Pipe_IN_cooled,power_consumption["inflow cooling"],_] = heat_exchanger_water2gas(Pipe_IN, 10, cfg.reactor_T_0_0)
+
+    Pipe_RE = State(recycle_estimate*total_mol_H2,
+                    recycle_estimate*total_mol_N2,
+                    0.5*total_mol_N2,
+                    cfg.reactor_T_0_0,
+                    cfg.reactor_P_0-2)
+
+    print(power_consumption)
+
+    stop = 0
+    while (stop == 0):
+
+        # Mix recycle stream in
+        Pipe_1a = mixer(Pipe_IN_cooled, Pipe_RE)
+
+
+        # recompress recycled stream
+        [Pipe_1b,power_consumption["recompressor"],_] = compressor(Pipe_1a,
+                                                                   p_out=cfg.reactor_P_0,
+                                                                   eta=cfg.recompressor_eta)
+
+
+        # Add heat from Low Temp Heat Exchanger to Pipe 1b to make Pipe 1c
+        Pipe_1c = copy.copy(Pipe_1b)
+        Pipe_1c.T += HTHE_DelT
+
+        [Pipe_1d,power_consumption["heater"]] = heater(Pipe_1c, cfg.reactor_T_1_0)
+
+
+        # Initialise bed data
+        Bed_data = []
+        Bed_iterator = copy.copy(Pipe_1d)
+        Bed_data.append(Bed_iterator.store())
+
+        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~ BED 1 ~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+        for X in range(bed1.vectlen - 1):
+            # setup new step
+            dX = bed1.vect[X + 1] - bed1.vect[X]
+
+            # run reactor step
+            Bed_iterator = reactorStep(Bed_iterator, dX, bed1.area)
+
+            # store data
+            Bed_data.append(Bed_iterator.store())
+
+
+
+        Pipe_2a = copy.copy(Bed_iterator)
+        Pipe_2a.p -= 2
+
+        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~ Heat exchanger 1 (Pipe 6 to Pipe 4) ~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+        # estimate heat exchange variant
+        [Pipe_2b, Pipe_1c_fake, HTHE_DelT_new, effectiveness_heex] = heat_exchanger_counter(Pipe_2a,
+                                                                                            Pipe_1b,
+                                                                                            effectiveness=cfg.he_counter_eff)
+        #print(effectiveness_heex)
+        HTHE_DelT_resid = abs(HTHE_DelT - HTHE_DelT_new)
+
+        HTHE_DelT = HTHE_DelT_new
+
+        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~ Heat exchanger 2 (outlet to water) ~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+        [Pipe_2c,power_consumption["Chiller"],effectiveness_cool] = heat_exchanger_water2gas(Pipe_2b,
+                                                                                             e1=cfg.he_water2gas_eff)
+        #print(effectiveness_cool)
+        #print('\n')
+        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~ Condensor ~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        [Pipe_RE, power_consumption["Condensor"]] = condensor(Pipe_2c,
+                                                              e2=cfg.condensor_eff,
+                                                              water_mass_flow=cfg.condensor_water_mfr)
+
+        print(Pipe_RE.yH2)
+        if HTHE_DelT_resid < cfg.reactor_convergence:
+            stop = 1
+
+
+    print('\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n')
+    # Mix recycle stream in
+    print('New Feed (cooled) = %3.1f' % Pipe_IN_cooled.T, 'K')
+    print('Recycle = %3.1f' % Pipe_RE.T, 'K')
+
+    print('Mixed with Recycle = %3.1f' % Pipe_1a.T, 'K')
+
+    # recompress recycled stream
+
+    print('Recompressed = %3.1f' % Pipe_1b.T, 'K')
+
+    # Add heat from Low Temp Heat Exchanger to Pipe 1b to make Pipe 1c
+
+    print('Reheated stream = %3.1f' % Pipe_1c.T, 'K')
+    print('heated to 663K stream = %3.1f' % Pipe_1d.T, 'K')
+
+    print('Bed 1 length = %2.2fm, conversion = %2.2f' % (bed1.vect[-1], Bed_iterator.NH3/(2*Pipe_1c.N2)*100) + '%' + ', T = %3.1f' % Bed_iterator.T + 'K')
+
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~ Heat exchanger 1 (Pipe 6 to Pipe 4) ~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    print('Immediately post reactor = %3.1f' % Pipe_2a.T, 'K')
+
+    print('Single cooled post reactor = %3.1f' % Pipe_2b.T,
+        'K\n Post HE Inlet into Reactor = %3.1f' % Pipe_1c_fake.T,
+        'K\n HTHE del T = %3.1f' % HTHE_DelT_new, ' Resid = %3.1f' % HTHE_DelT_resid)
+
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~ Heat exchanger 2 (outlet to water) ~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+    print('Double cooled chiller outlet T = %3.1f' % Pipe_2c.T)
+
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~ Condensor ~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~ Recycle ~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # reheat recycle stream? loosing too much energy rn
+    print('Ammonia produced = %2.4f g/s' % float((Pipe_2c.mNH3-Pipe_RE.mNH3)*1e3))
+
+
+    recycle_ratio_mol = (Pipe_RE.mol_tot / Pipe_IN.mol_tot)
+    recycle_ratio_mass = (Pipe_RE.mass_tot / Pipe_IN.mass_tot)
+    print('recycle ratio mass = %2.3f' % recycle_ratio_mass, ', recycle ratio mol = %2.3f' % recycle_ratio_mol)
+
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~ Heat exchanger 1 (pt 2?) ~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    print('power consumption = ', power_consumption)
+
+    print('\n Stream data :')
+    Bed_data_T = np.array(Bed_data).T.tolist()
+
+    print(Pipe_IN_cooled.store())
+    print(Pipe_1a.store())
+    print(Pipe_1b.store())
+    print(Pipe_1c.store())
+    print(Pipe_2a.store())
+    print(Pipe_2b.store())
+    print(Pipe_2c.store())
+    print(Pipe_RE.store())
+    plot = 1
+    if plot:
+        x_plot_data = bed1.vect
+
+        y_plot_data = Bed_data_T[3]
+
+
+        plt.plot(x_plot_data,y_plot_data)
+        plt.show()
+
+    # print(bedvect)
+
+
+if __name__ == "__main__":
+    main()
--- utils/options.py
+++ utils/options.py
@@ -1,169 +1,38 @@
 '''
 This file will handle all interaction with the options configuration file.
 '''
-
 import os
+import configparser
 
-class Options:
+class Config:
     """
-    An options class to store and handle all options for fitbenchmarking
+    A config class to store and handle all options for ammonia-plant
     """
 
-    DEFAULTS = os.path.abspath(os.path.join(os.path.dirname(__file__),
-                                            'default_options.ini'))
-    VALID_SECTIONS = ['MINIMIZERS', 'FITTING', 'JACOBIAN',
-                      'PLOTTING', 'LOGGING']
-    VALID_MINIMIZERS = \
-        {'bumps': ['amoeba', 'lm-bumps', 'newton', 'de', 'mp'],
-         'dfo': ['dfogn', 'dfols'],
-         'gradient_free': ['HillClimbingOptimizer',
-                           'RepulsingHillClimbingOptimizer',
-                           'SimulatedAnnealingOptimizer',
-                           'RandomSearchOptimizer',
-                           'RandomRestartHillClimbingOptimizer',
-                           'RandomAnnealingOptimizer',
-                           'ParallelTemperingOptimizer',
-                           'ParticleSwarmOptimizer',
-                           'EvolutionStrategyOptimizer',
-                           'BayesianOptimizer',
-                           'TreeStructuredParzenEstimators',
-                           'DecisionTreeOptimizer'],
-         'gsl': ['lmsder', 'lmder', 'nmsimplex', 'nmsimplex2',
-                 'conjugate_pr', 'conjugate_fr', 'vector_bfgs',
-                 'vector_bfgs2', 'steepest_descent'],
-         'levmar': ['levmar'],
-         'mantid': ['BFGS',
-                    'Conjugate gradient (Fletcher-Reeves imp.)',
-                    'Conjugate gradient (Polak-Ribiere imp.)',
-                    'Damped GaussNewton', 'Levenberg-Marquardt',
-                    'Levenberg-MarquardtMD', 'Simplex',
-                    'SteepestDescent', 'Trust Region'],
-         'matlab': ['Nelder-Mead Simplex'],
-         'matlab_curve': ['Levenberg-Marquardt', 'Trust-Region'],
-         'matlab_opt': ['levenberg-marquardt', 'trust-region-reflective'],
-         'matlab_stats': ['Levenberg-Marquardt'],
-         'minuit': ['minuit'],
-         'ralfit': ['gn', 'gn_reg', 'hybrid', 'hybrid_reg'],
-         'scipy': ['Nelder-Mead', 'Powell', 'CG', 'BFGS',
-                   'Newton-CG', 'L-BFGS-B', 'TNC', 'SLSQP'],
-         'scipy_ls': ['lm-scipy', 'trf', 'dogbox'],
-         'scipy_go': ['differential_evolution', 'shgo', 'dual_annealing']}
-    VALID_FITTING = \
-        {'algorithm_type': ['all', 'ls', 'deriv_free', 'general', 'simplex',
-                            'trust_region', 'levenberg-marquardt',
-                            'gauss_newton', 'bfgs', 'conjugate_gradient',
-                            'steepest_descent', 'global_optimization'],
-         'software': ['bumps', 'dfo', 'gradient_free', 'gsl', 'levmar',
-                      'mantid', 'matlab', 'matlab_curve', 'matlab_opt',
-                      'matlab_stats', 'minuit', 'ralfit', 'scipy',
-                      'scipy_ls', 'scipy_go'],
-         'jac_method': ['scipy', 'analytic', 'default', 'numdifftools'],
-         'cost_func_type': ['nlls', 'weighted_nlls', 'hellinger_nlls',
-                            'poisson']}
-    VALID_JACOBIAN = \
-        {'scipy': ['2-point', '3-point', 'cs'],
-         'analytic': ['cutest'],
-         'default': ['default'],
-         'numdifftools': ['central',
-                          'complex', 'multicomplex',
-                          'forward', 'backward']}
-    VALID_PLOTTING = \
-        {'make_plots': [True, False],
-         'comparison_mode': ['abs', 'rel', 'both'],
-         'table_type': ['acc', 'runtime', 'compare', 'local_min'],
-         'colour_map': plt.colormaps()}
-    VALID_LOGGING = \
-        {'level': ['NOTSET', 'DEBUG', 'INFO', 'WARNING', 'ERROR',
-                   'CRITICAL'],
-         'append': [True, False],
-         'external_output': ['debug', 'display', 'log_only']}
+    # DEFAULTS: to be driven by a static .ini file stored in utils
 
-    VALID = {'MINIMIZERS': VALID_MINIMIZERS,
-             'FITTING': VALID_FITTING,
-             'JACOBIAN': VALID_JACOBIAN,
-             'PLOTTING': VALID_PLOTTING,
-             'LOGGING': VALID_LOGGING}
+    VALID_SECTIONS = ['reactor', 'n2compressor', 'h2compressor',
+                      'recompressor', 'heater', 'mixer', 'heat_exchanger_counter',
+                      'heat_exchanger_water2gas', 'condensor']
 
-    DEFAULT_MINIMZERS = \
-        {'bumps': ['amoeba', 'lm-bumps', 'newton', 'mp'],
-         'dfo': ['dfogn', 'dfols'],
-         'gradient_free': ['HillClimbingOptimizer',
-                           'RepulsingHillClimbingOptimizer',
-                           'SimulatedAnnealingOptimizer',
-                           'RandomSearchOptimizer',
-                           'RandomRestartHillClimbingOptimizer',
-                           'RandomAnnealingOptimizer',
-                           'ParallelTemperingOptimizer',
-                           'ParticleSwarmOptimizer',
-                           'EvolutionStrategyOptimizer'],
-         'gsl': ['lmsder', 'lmder', 'nmsimplex', 'nmsimplex2',
-                 'conjugate_pr', 'conjugate_fr', 'vector_bfgs',
-                 'vector_bfgs2', 'steepest_descent'],
-         'levmar': ['levmar'],
-         'mantid': ['BFGS',
-                    'Conjugate gradient (Fletcher-Reeves imp.)',
-                    'Conjugate gradient (Polak-Ribiere imp.)',
-                    'Damped GaussNewton', 'Levenberg-Marquardt',
-                    'Levenberg-MarquardtMD', 'Simplex',
-                    'SteepestDescent', 'Trust Region'],
-         'matlab': ['Nelder-Mead Simplex'],
-         'matlab_curve': ['Levenberg-Marquardt', 'Trust-Region'],
-         'matlab_opt': ['levenberg-marquardt', 'trust-region-reflective'],
-         'matlab_stats': ['Levenberg-Marquardt'],
-         'minuit': ['minuit'],
-         'ralfit': ['gn', 'gn_reg', 'hybrid', 'hybrid_reg'],
-         'scipy': ['Nelder-Mead', 'Powell', 'CG', 'BFGS',
-                   'Newton-CG', 'L-BFGS-B', 'TNC', 'SLSQP'],
-         'scipy_ls': ['lm-scipy', 'trf', 'dogbox'],
-         'scipy_go': ['differential_evolution', 'shgo', 'dual_annealing']}
-    DEFAULT_FITTING = \
-        {'num_runs': 5,
-         'algorithm_type': ['all'],
-         'software': ['scipy', 'scipy_ls'],
-         'jac_method': ['scipy'],
-         'cost_func_type': 'weighted_nlls'}
-    DEFAULT_JACOBIAN = \
-        {'analytic': ['cutest'],
-         'scipy': ['2-point'],
-         'default': ['default'],
-         'numdifftools': ['central']}
-    DEFAULT_PLOTTING = \
-        {'make_plots': True,
-         'colour_map': 'magma_r',
-         'colour_ulim': 100,
-         'cmap_range': [0.2, 0.8],
-         'comparison_mode': 'both',
-         'table_type': ['acc', 'runtime', 'compare', 'local_min'],
-         'results_dir': 'fitbenchmarking_results'}
-    DEFAULT_LOGGING = \
-        {'file_name': 'fitbenchmarking.log',
-         'append': False,
-         'level': 'INFO',
-         'external_output': 'log_only'}
-    DEFAULTS = {'MINIMIZERS': DEFAULT_MINIMZERS,
-                'FITTING': DEFAULT_FITTING,
-                'JACOBIAN': DEFAULT_JACOBIAN,
-                'PLOTTING': DEFAULT_PLOTTING,
-                'LOGGING': DEFAULT_LOGGING}
+    #VALID = {'MINIMIZERS': VALID_MINIMIZERS,
+    #         'FITTING': VALID_FITTING,
+    #         'JACOBIAN': VALID_JACOBIAN,
+    #         'PLOTTING': VALID_PLOTTING,
+    #         'LOGGING': VALID_LOGGING}
+    # At a later date we could add valid ranges
+
 
     def __init__(self, file_name=None):
         """
-        Initialise the options from a file if file is given.
-        Priority is values in the file, failing that, values are taken from
-        DEFAULTS (stored in ./default_options.ini)
-
-        :param file_name: The options file to load
-        :type file_name: str
+        Initialise the options from a given file.
         """
         # stores the file name to be used to reset options for multiple
         # problem groups
         self.stored_file_name = file_name
         self.error_message = []
         self._results_dir = ''
-        config = configparser.ConfigParser(converters={'list': read_list,
-                                                       'str': str,
-                                                       'rng': read_range},
-                                           allow_no_value=True)
+        config = configparser.ConfigParser(inline_comment_prefixes="#")
 
         for section in self.VALID_SECTIONS:
             config.add_section(section)
@@ -171,64 +40,29 @@ class Options:
         if file_name is not None:
             config.read(file_name)
 
-            # Checks that the user defined sections are valid
-            if config.sections() != self.VALID_SECTIONS:
-                raise OptionsError(
-                    "Invalid options sections set, {0}, the valid sections "
-                    "are {1}".format(config.sections(), self.VALID_SECTIONS))
-            config.sections()
+        self.reactor_T_0_0 = config.getfloat('reactor','T_0_0')
+        self.reactor_T_1_0 = config.getfloat('reactor','T_1_0')
+        self.reactor_P_0 = config.getfloat('reactor','P_0')
+        self.reactor_length = config.getfloat('reactor','length')
+        self.reactor_diameter = config.getfloat('reactor','diameter')
+        self.reactor_mini = config.getfloat('reactor','mini')
+        self.reactor_convergence = config.getfloat('reactor','convergence')
+        self.n2 = config.getfloat('reactor','n2')
+        self.h2 = config.getfloat('reactor','h2')
+        self.reactor_dT_res = config.getfloat('reactor','dT_res')
+        self.recycle = config.getfloat('reactor','recycle')
 
-            # Checks that the options within the sections are valid
-            for key in self.VALID_SECTIONS:
-                default_options_list = list(self.DEFAULTS[key].keys())
-                user_options_list = [option[0] for option in config.items(key)]
-                if not set(user_options_list) <= set(default_options_list):
-                    raise OptionsError(
-                        "Invalid options key set in the {2} Section: \n{0}, \n"
-                        " the valid keys are: \n{1}".format(
-                            user_options_list,
-                            default_options_list,
-                            key))
 
-        minimizers = config['MINIMIZERS']
-        self._minimizers = {}
-        self.minimizer_alg_type = {}
-        for key in self.VALID_FITTING["software"]:
-            self._minimizers[key] = self.read_value(minimizers.getlist,
-                                                    key)
+        self.n2compressor_dT = config.getfloat('n2compressor','dT')
 
-        fitting = config['FITTING']
-        self.num_runs = self.read_value(fitting.getint, 'num_runs')
-        self.algorithm_type = self.read_value(
-            fitting.getlist, 'algorithm_type')
-        self.software = self.read_value(fitting.getlist, 'software')
-        self.jac_method = self.read_value(fitting.getlist, 'jac_method')
-        self.cost_func_type = self.read_value(fitting.getstr, 'cost_func_type')
+        self.h2compressor_dT = config.getfloat('h2compressor','dT')
 
-        jacobian = config['JACOBIAN']
-        self.num_method = {}
-        for key in self.VALID_FITTING["jac_method"]:
-            self.num_method[key] = self.read_value(jacobian.getlist,
-                                                   key)
+        self.recompressor_eta = config.getfloat('recompressor','eta')
 
-        plotting = config['PLOTTING']
-        self.make_plots = self.read_value(plotting.getboolean, 'make_plots')
-        self.colour_map = self.read_value(plotting.getstr, 'colour_map')
-        self.colour_ulim = self.read_value(plotting.getfloat, 'colour_ulim')
-        self.cmap_range = self.read_value(plotting.getrng, 'cmap_range')
+        self.he_counter_eff = config.getfloat('he_counter','eff')
 
-        self.comparison_mode = self.read_value(plotting.getstr,
-                                               'comparison_mode')
-        self.table_type = self.read_value(plotting.getlist, 'table_type')
-        self.results_dir = self.read_value(plotting.getstr, 'results_dir')
+        self.he_water2gas_eff = config.getfloat('he_water2gas','eff')
 
-        logging = config['LOGGING']
-        self.log_append = self.read_value(logging.getboolean, 'append')
-        self.log_file = self.read_value(logging.getstr, 'file_name')
-        self.log_level = self.read_value(logging.getstr, 'level')
-
-        self.external_output = self.read_value(logging.getstr,
-                                               'external_output')
-
-        if self.error_message != []:
-            raise OptionsError('\n'.join(self.error_message))
+        self.condensor_water_mfr = config.getfloat('condensor','water_mfr')
+        self.condensor_eff = config.getfloat('condensor','eff')
+        
